2bdc3f9af346b8f614171ecc5e8d087d4293c0e3
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-05-01 15:26:07 -0500

    updates to table

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(45),
-    name VARCHAR(45),
-    release_date DATE NOT NULL,
-    sales FLOAT(7, 2) UNSIGNED NOT NULL,
-    genre VARCHAR(35),
+    artist VARCHAR(50) NOT NULL ,
+    name VARCHAR(100) NOT NULL ,
+    release_date INT NOT NULL,
+    sales FLOAT(15, 2) UNSIGNED NOT NULL,
+    genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
