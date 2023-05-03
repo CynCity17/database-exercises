9cc3f5333392b13f99dc8b45416eea7b8c417ac7
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-05-03 14:48:53 -0500

    adding functions exercises for SQL

---
+++ functions_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no DESC;
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE month(birth_date) = 12
+                          AND day(birth_date) = 25;
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                          AND month(birth_date) = 12
+                          AND day(birth_date) = 25
+                        ORDER BY birth_date, hire_date DESC;
+SELECT DATEDIFF(NOW(), hire_date) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999
+                          AND month(birth_date) = 12
+                          AND day(birth_date) = 25;
